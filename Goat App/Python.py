eeb53613f3585508d6d322d46f4770d4ce8f791a
Rishab Kanoujia <108139937+ArtistRK@users.noreply.github.com>
2024-11-25 22:40:38 -0500

    Update Python.py

--- Goat App/Python.py
+++ Goat App/Python.py
@@ -3,7 +3,7 @@ name = input("Bakra tera nam ki aa: ")
 
 age = input("Ho kina da gea aa: ")
 
-print(f"Hanji {name} ustadd ji, tuc {age} hoga aa")
+print(f"Hanji {name} ustadd ji, tuc {age} hoga a")
 
 Print ("Hello World")
 
